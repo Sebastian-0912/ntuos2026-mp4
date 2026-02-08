d83d07abdf183b8792a90bcbf76dc48b8dee01b3
Tzu-Ching Yang <shingekinocore@gmail.com>
2026-02-08 15:48:53 +0800

    chore(template): remove unused legacy test files

--- tests/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules=cythonize(
-        [], # Add .pyx files here
-        compiler_directives={'language_level': '3'}
-    )
-)
--- tests/test_loader.py
+++
@@ -1,7 +0,0 @@
-# from run_mp2 import public_testcases
-# import os
-
-# Register tests
-# Note: run_mp2.py functions return the test function, but calling them
-# inside @test decorator (which they use) registers them in gradelib.TESTS
-# public_testcases(range(25))
